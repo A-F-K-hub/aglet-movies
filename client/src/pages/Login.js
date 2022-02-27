fe8a7c1f98ec77c748ee6a7309d0611139470eb1
hyacinthe <hyacinthe@codehesion.co.za>
2022-02-28 01:17:54 +0200

    small fix

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { Link, useNavigate} from "react-router-dom";
 import userApiService from "../api-services/users-api/user.api-service";
 import Navbar from "../Navbar";
+import { Context } from "../context/Context";
 import { setCurrentUserAction, setIsLoadingGetCurrentUserAction } from "../reducers/users-reducer/users.reducer";
 import "./Login.css";
 
@@ -11,30 +12,31 @@ import "./Login.css";
 function Login() {
   const userRef = useRef();
   const passwordRef = useRef();
-  const dispatch = useDispatch();
+  const { dispatch, isFetching } = useContext(Context);
+  const reduxDispatch = useDispatch();
   const navigate = useNavigate();
   const [loginLoading, setLoginLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // dispatch({ type: "LOGIN_START" });
+    dispatch({ type: "LOGIN_START" });
     try {
       setLoginLoading(true);
       const loginResponse = await axios.post("/auth/login", {
         username: userRef.current.value,
         password: passwordRef.current.value,
       });
-    
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
       navigate('/');
-      dispatch(setIsLoadingGetCurrentUserAction(true));
+      reduxDispatch(setIsLoadingGetCurrentUserAction(true));
       userApiService.getCurrentUser(loginResponse.data._id).then((response)=>{
-        dispatch(setCurrentUserAction(response));
+        reduxDispatch(setCurrentUserAction(response));
       }).finally(()=>{
-        dispatch(setIsLoadingGetCurrentUserAction(false))
+        reduxDispatch(setIsLoadingGetCurrentUserAction(false))
       })
 
     } catch (err) {
-      // dispatch({ type: "LOGIN_FAILURE" });
+      dispatch({ type: "LOGIN_FAILURE" });
     }
     finally{
       setLoginLoading(false);
